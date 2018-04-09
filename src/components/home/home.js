16008ff94a6b8f9b9ec22f663b0010f173e97a00
briandhkim <w1057216@gmail.com>
2018-04-09 01:42:37 -0700

    data pull checked

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,18 +1,38 @@
 import React, {Component} from 'react';
-
+import axios from 'axios';
 import {PageHeader, Table} from 'react-bootstrap';
 
 class Home extends Component{
 	constructor(props){
 		super(props);
 		this.state={};
+		this.getEmployeeData = this.getEmployeeData.bind(this);
+	}
+
+	componentDidMount(){
+		this.getEmployeeData();
+	}
+
+	getEmployeeData(){
+		const apiURL = 'http://localhost:8080/HouseCoffee/get_all';
+		const request = axios.get(apiURL)
+			.then((res)=>{
+				const dataStr = res.data;
+				const qChange = dataStr.replace(/'/g,'"');
+				const dataArr = JSON.parse(qChange);
+				
+				console.log(dataArr, typeof dataArr);
+			})
+			.catch((err)=>{
+				console.log(err);
+			});
 	}
 
 	render(){
 		
 		return(
 			<div className='container-fluid'>
-				<PageHeader className='col-xs-12 col-sm-10 col-sm-offset-1'>
+				<div className='page-header col-xs-12 col-sm-10 col-sm-offset-1'>
 					<h1 className='hidden-xs h1-responsive'>
 						<strong>Employee Table</strong>
 						<small className='pull-right'>Paddy's Pub</small>
@@ -21,7 +41,7 @@ class Home extends Component{
 						<strong>Employee Table</strong>
 						<small className='pull-right'>Paddy's Pub</small>
 					</h3>
-				</PageHeader>
+				</div>
 
 				<div className='form-horizontal col-sm-2 col-sm-push-9'>
 					<h4><strong>Add Employee</strong></h4>
